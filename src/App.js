a5f4f29eb95600859bebde820cae418f233ea1db
pungitoregerman <pungitoregerman@gmail.com>
2022-11-04 18:40:08 -0300

    Pagina y componente notfound, enrutados home y notfound

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import Home from "./pages/home/Home";
 import "./App.css";
-/* import { Routes,Route } from "react-router-dom"; */
+import NotFound from "./pages/NotFound/NotFound";
+import { Routes,Route } from "react-router-dom"; 
 
 function App() {
   return (<>
-  <Home />
-  
-
+  <Routes>
+    <Route path='/' element={<Home/>}/> 
+    <Route path='*' element={<NotFound/>}/>
+  </Routes> 
   </>
   )
-
 }
-
 export default App;
-
-
-/*   <Routes>
-    <Route path='/index' element={}/>
-  </Routes> */
--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -10,3 +10,4 @@ export default function CallToAction(props) {
       </div>
   )
 }
+
