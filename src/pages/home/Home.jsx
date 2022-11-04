40eb163c262703fea4b129c798c8e26b573ad429
jonyolmeda <jonyolmeda@gmail.com>
2022-11-04 17:24:29 -0300

    Corrección de Layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import LayoutBase from "../../layouts/LayoutBase";
 import "./home.css";
 import Home1 from "../../components/home1/Home1";
 import Home2 from "../../components/home2/Home2";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Header />
-      <Home1 />
-      <Home2 />
-      <Footer />
-    </div>
-  );
-}
+    <LayoutBase>
+      <div className="container">
+      {<Home1 />}
+      {<Home2 />}
+      </div>
+    </LayoutBase>
+
+
+)}
+
