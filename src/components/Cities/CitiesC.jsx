919443069a65d3fd0498bca2e84770157a6b7ce1
jonyolmeda <jonyolmeda@gmail.com>
2022-11-08 21:35:14 -0300

    Correcciones de diseño

--- src/components/Cities/CitiesC.jsx
+++ src/components/Cities/CitiesC.jsx
@@ -13,7 +13,7 @@ export default function Cities() {
       <div className='container-input-cards'>
         <div className='container-plus'>
           <input className='input-cards' ref={search} id='search' type="search" placeholder='Search city...' />
-          <a href="/newcity"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"></path></svg></a>
+          <a className='add-city' href="/newcity">Add new city</a>
           
         </div>
       <div className='checkbox-cards'>
